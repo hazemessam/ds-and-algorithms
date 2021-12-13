5cfb9c69e3955900582e53d19b610ad810ec9c78
Hazem Essam <hazemkwita123@gmail.com>
2021-12-13 10:04:57 +0200

    make qsort use random pivot element instead of the first element

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def qsort(arr: list) -> list:
     if len(arr) < 2: return arr
     if len(arr) == 2:
@@ -7,17 +10,23 @@ def qsort(arr: list) -> list:
             arr[1] = temp
         return arr
     
-    pivot = arr[0]
+    pivot_idx = randint(0, len(arr)-1)
+    pivot = arr[pivot_idx]
     left_sub_arr = []
     right_sub_arr = []
-    for el in arr[1:]:
+    
+    for el in arr[:pivot_idx]:
         if el <= pivot: left_sub_arr.append(el)
         else: right_sub_arr.append(el)
-
+        
+    for el in arr[pivot_idx+1:]:
+        if el <= pivot: left_sub_arr.append(el)
+        else: right_sub_arr.append(el)
+        
     return qsort(left_sub_arr) + [pivot] + qsort(right_sub_arr)
 
 
-print(qsort([1, 2, 3, 4, 6]))
+print(qsort([1, 2, 3, 4, 6, 7, 8]))
 print(qsort([1, 6, 3, 4, 5]))
 print(qsort([]))
 print(qsort([1]))
